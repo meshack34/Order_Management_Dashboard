29cb791f457ace547feb1bee8f8d064fd46eea44
meshack34 <meshackkimutai34@gmail.com>
2025-04-30 12:55:26 +0300

    set up db

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/orders/orders.routes').then(m => m.ORDER_ROUTES)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
