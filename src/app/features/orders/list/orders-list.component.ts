8a8577bd20e3e5f92426d8236af573a2b8a16fc6
meshack34 <meshackkimutai34@gmail.com>
2025-04-30 14:08:10 +0300

    Create Orders List Component

---
+++ src/app/features/orders/list/orders-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-list',
+  imports: [],
+  templateUrl: './orders-list.component.html',
+  styleUrl: './orders-list.component.scss'
+})
+export class OrdersListComponent {
+
+}
